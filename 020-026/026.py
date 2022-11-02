3e3eb2a29a1ffb7e87875e4b00e2b0ebff361729
Ascetholix <sangak@mail.ru>
2022-11-02 20:31:36 +0300

    add decision 020-026

--- 020-026/026.py
+++ 020-026/026.py
@@ -5,3 +5,28 @@
 #Напишите программу, которая предлагает пользователю ввести слово и преобразует
 #его в «поросячью латынь».
 #Проследите за тем, чтобы новое слово выводилось в нижнем регистре.
+# A, E, I, O, U, Y
+
+text = input("Введите слово: ")
+text = text.lower()
+first = text[0]
+n = len(text)
+
+# if(first =="a" or first == "i" or first == "y" ):
+#   text = text + "way"
+#   print(text)
+# elif(first == "e" or first == "o" or first== "u" ):
+#   text = text + "way"
+#   print(text)
+# else:   
+#   text = text[1:n] + first + "ay"
+#   print(text)
+
+if(first !="a" and first !="i" and first !="y" and first !="e" and first !="o" and first !="u"):
+  text = text[1:n] + first + "ay"
+  print(text)
+else:
+  text = text + "way"
+  print(text)
+  
+ 
