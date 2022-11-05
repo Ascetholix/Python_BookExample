4e853c75bfa0369f0798a8675391eb1937db76e2
Ascetholix <sangak@mail.ru>
2022-11-05 20:05:29 +0300

    add all task

---
+++ 045-051/051.py
@@ -0,0 +1,9 @@
+# Выведите строки «There are [счетчик] green bottles hanging on the wall, [счетчик] green bottles 
+# hanging on the wall, and if 1 green bottle should accidentally fall».
+# Затем выведите вопрос: «how many green bottles will be hanging on the wall?».
+# Если пользователь ответит правильно, выведите сообщение 
+# «There will be [счетчик] green bottles hanging on the wall».
+# Если пользователь ответит неправильно, выведите сообщение «No, try again»,
+# пока не будет дан правильный ответ.
+# Когда счетчик уменьшится до 0, выведите сообщение 
+# «There are no more green bottles hanging on the wall»
